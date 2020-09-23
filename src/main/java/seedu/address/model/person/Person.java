86b3b045ad02557a9cfa8a84c274b45f08096abe
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-09-23 12:34:14 +0800

    Implement new property - infection status
    This commit only implements the new property of infection status
    for a person. The program does not compile at this stage.
    
    Refactoring has to be done to ensure that this new property is
    successfully integrated into the project.

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -22,17 +22,20 @@ public class Person {
 
     // Data fields
     private final Address address;
+    private final InfectionStatus infectionStatus;
     private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+    public Person(Name name, Phone phone, Email email,
+                  Address address, InfectionStatus infectionStatus, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
+        this.infectionStatus = infectionStatus;
         this.tags.addAll(tags);
     }
 
@@ -50,6 +53,10 @@ public class Person {
 
     public Address getAddress() {
         return address;
+    }
+
+    public InfectionStatus getInfectionStatus() {
+        return infectionStatus;
     }
 
     /**
@@ -93,13 +100,14 @@ public class Person {
                 && otherPerson.getPhone().equals(getPhone())
                 && otherPerson.getEmail().equals(getEmail())
                 && otherPerson.getAddress().equals(getAddress())
+                && otherPerson.getInfectionStatus().equals(getInfectionStatus())
                 && otherPerson.getTags().equals(getTags());
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, tags);
+        return Objects.hash(name, phone, email, address, infectionStatus, tags);
     }
 
     @Override
@@ -112,6 +120,8 @@ public class Person {
                 .append(getEmail())
                 .append(" Address: ")
                 .append(getAddress())
+                .append(" Infected: ")
+                .append(getInfectionStatus())
                 .append(" Tags: ");
         getTags().forEach(builder::append);
         return builder.toString();
