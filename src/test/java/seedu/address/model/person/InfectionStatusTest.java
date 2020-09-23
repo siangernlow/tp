21548ef755ac6ac2f79bce7d24a8b5d2ce188dfa
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-09-23 18:11:02 +0800

    Improve code layout

--- src/test/java/seedu/address/model/person/InfectionStatusTest.java
+++ src/test/java/seedu/address/model/person/InfectionStatusTest.java
@@ -7,6 +7,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class InfectionStatusTest {
+
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new InfectionStatus(null));
