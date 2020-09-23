06ada516ec7e8d628e89950bb7eaa09bb2d52a31
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-09-23 17:13:15 +0800

    Refactor tests to include infection status

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.InfectionStatus;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -20,11 +21,13 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "alice@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_INFECTION_STATUS = "false";
 
     private Name name;
     private Phone phone;
     private Email email;
     private Address address;
+    private InfectionStatus infectionStatus;
     private Set<Tag> tags;
 
     /**
@@ -35,6 +38,7 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
+        infectionStatus = new InfectionStatus(DEFAULT_INFECTION_STATUS);
         tags = new HashSet<>();
     }
 
@@ -46,6 +50,7 @@ public class PersonBuilder {
         phone = personToCopy.getPhone();
         email = personToCopy.getEmail();
         address = personToCopy.getAddress();
+        infectionStatus = personToCopy.getInfectionStatus();
         tags = new HashSet<>(personToCopy.getTags());
     }
 
@@ -89,8 +94,19 @@ public class PersonBuilder {
         return this;
     }
 
+    /**
+     * Sets the {@code InfectionStatus} of the {@code Person} that we are building.
+     *
+     * @param infectionStatus The new infection status.
+     * @return The updated PersonBuilder.
+     */
+    public PersonBuilder withInfectionStatus(String infectionStatus) {
+        this.infectionStatus = new InfectionStatus(infectionStatus);
+        return this;
+    }
+
     public Person build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, infectionStatus, tags);
     }
 
 }
