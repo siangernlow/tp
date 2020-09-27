986f49ee531f734805884069900916997ba0eb4e
HoPinXian <hopinxian@gmail.com>
2020-09-27 12:19:59 +0800

    Update locations exceptions format
    Exception classes updated to pass checkstyle.

--- src/main/java/seedu/address/model/location/exceptions/DuplicateLocationException.java
+++ src/main/java/seedu/address/model/location/exceptions/DuplicateLocationException.java
@@ -1,8 +1,8 @@
 package seedu.address.model.location.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Locations (Locations are considered duplicates if they have the same
- * identity).
+ * Signals that the operation will result in duplicate Locations (Locations are considered duplicates if they have
+ * the same identity).
  */
 public class DuplicateLocationException extends RuntimeException {
     public DuplicateLocationException() {
