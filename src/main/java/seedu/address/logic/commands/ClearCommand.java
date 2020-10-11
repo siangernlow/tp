757d19c5daaa2f45d4cd7b6f60391ce25f53845a
HoPinXian <hopinxian@gmail.com>
2020-10-11 18:41:33 +0800

    Update Clear command to clear all books
    Clear command previously only clears person book.
    It has been updated to clear location book, person book
    and visit book.

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,22 +2,26 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import seedu.address.model.LocationBook;
 import seedu.address.model.Model;
 import seedu.address.model.PersonBook;
+import seedu.address.model.VisitBook;
 
 /**
- * Clears the address book.
+ * Clears the VirusTracker.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "VirusTracker has been cleared!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new PersonBook());
+        model.setLocationBook(new LocationBook());
+        model.setVisitBook(new VisitBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
