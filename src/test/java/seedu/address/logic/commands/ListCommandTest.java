4eabbfd1acacd62c8fe248202739d90addd4d31c
HoPinXian <hopinxian@gmail.com>
2020-10-11 22:01:51 +0800

    Update test for showing statistics

--- src/test/java/seedu/address/logic/commands/ListCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ListCommandTest.java
@@ -84,8 +84,16 @@ public class ListCommandTest {
 
     @Test
     public void execute_statistics_showsSameList() {
+        String expectedMessage = ListCommand.MESSAGE_SUCCESS_STATISTICS + "\n"
+                + "Total number of people: 7" + "\n"
+                + "Total number of locations: 7" + "\n"
+                + "Total number of visits: 8" + "\n"
+                + "Total number of infected people: 4" + "\n"
+                + "Total number of quarantined people: 2" + "\n"
+                + "Percentage of people infected: 57.14%" + "\n"
+                + "Percentage of people quarantined: 28.57%" + "\n";
         assertCommandSuccess(new ListCommand(STATISTICS_LIST),
-                model, ListCommand.MESSAGE_SUCCESS_STATISTICS, expectedModel);
+                model, expectedMessage, expectedModel);
     }
 
     @Test
