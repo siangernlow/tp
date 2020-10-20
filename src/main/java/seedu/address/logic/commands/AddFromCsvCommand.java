9ad1b04efff08058feb59df4825b1455913f92e8
siangernlow <65005806+siangernlow@users.noreply.github.com>
2020-10-20 18:53:28 +0800

    Improve readability of command usage message

--- src/main/java/seedu/address/logic/commands/AddFromCsvCommand.java
+++ src/main/java/seedu/address/logic/commands/AddFromCsvCommand.java
@@ -9,7 +9,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_LIST;
 public abstract class AddFromCsvCommand extends Command {
     public static final String COMMAND_WORD = "addFromCsv";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Reads and adds the data from the specified CSV file "
-            + "to VirusTracker. "
+            + "to VirusTracker.\n"
             + "Parameters: FILE_PATH "
             + PREFIX_LIST + "DATA_TYPE\n"
             + "FILE_PATH is the absolute file path of the .csv file.\n"
@@ -17,7 +17,7 @@ public abstract class AddFromCsvCommand extends Command {
             + "The CSV file should have columns corresponding to each parameter required "
             + "in the respective add functions. Refer to the user guide for more details.\n"
             + "Example: " + COMMAND_WORD + " "
-            + "C:/Users/admin/peopleData.csv l/people";
+            + "C:/Users/admin/peopleData.csv " + PREFIX_LIST + "people";
     public static final String MESSAGE_DUPLICATES_NOT_ADDED = "Duplicate %1$s were detected on the "
             + "following lines:\n"
             + "%2$s\n "
