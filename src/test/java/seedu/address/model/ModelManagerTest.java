cb9806d3ebc881903a77c305b884b36e09947526
hhdqirui <53338059+hhdqirui@users.noreply.github.com>
2020-10-21 18:28:00 +0800

    Use assertion in ModelManagerTest

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -253,6 +253,9 @@ public class ModelManagerTest {
         Location editedLocation = new LocationBuilder(BENSON_LOCATION).withName("benson location")
                 .build();
         Visit editedVisit = new VisitBuilder(FIRST_VISIT).withLocation(editedLocation).build();
+
+        assert !FIRST_VISIT.equals(editedVisit);
+
         expectedModel.setVisit(FIRST_VISIT, editedVisit);
         actualModel.updateVisitBookWithEditedLocation(editedLocation);
         assertEquals(expectedModel, actualModel);
@@ -260,6 +263,10 @@ public class ModelManagerTest {
         Location editedSecondLocation = new LocationBuilder(AMY_LOCATION).withName("amy location").build();
         Visit editedSecondVisit = new VisitBuilder(SECOND_VISIT).withLocation(editedSecondLocation).build();
         Visit editedThirdVisit = new VisitBuilder(THIRD_VISIT).withLocation(editedSecondLocation).build();
+
+        assert !SECOND_VISIT.equals(editedSecondVisit);
+        assert !THIRD_VISIT.equals(editedThirdVisit);
+
         expectedModel.setVisit(SECOND_VISIT, editedSecondVisit);
         expectedModel.setVisit(THIRD_VISIT, editedThirdVisit);
         actualModel.updateVisitBookWithEditedLocation(editedSecondLocation);
