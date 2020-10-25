f0c8b555550618076aca6e0139a060950a9b9633
HoPinXian <hopinxian@gmail.com>
2020-10-25 18:47:19 +0800

    Implement IndexIdPair

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -8,8 +8,8 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_DATE_FORMAT = "Invalid date format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_INVALID_LOCATION_DISPLAYED_INDEX = "The location index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid!";
+    public static final String MESSAGE_INVALID_LOCATION_DISPLAYED_INDEX = "The location index provided is invalid!";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_INVALID_PERSON_ID = "The person ID provided cannot be found!";
     public static final String MESSAGE_INVALID_LOCATION_ID = "The location ID provided cannot be found!";
---
+++ src/main/java/seedu/address/logic/parser/IndexIdPair.java
@@ -0,0 +1,125 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON_ID;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.Model;
+import seedu.address.model.attribute.Id;
+import seedu.address.model.location.Location;
+import seedu.address.model.person.Person;
+
+/**
+ * This represents an identifier that can identify a Person or a Location.
+ * The identifier may be using either an Id or an index from user input.
+ */
+public class IndexIdPair {
+
+    public static final String MESSAGE_INVALID_PERSON_COMMAND_USE = "This pair refers to a location, not a person.";
+    public static final String MESSAGE_INVALID_LOCATION_COMMAND_USE = "This pair refers to a person, not a location.";
+
+    private Optional<Index> indexOpt;
+    private Optional<Id> idOpt;
+    private Prefix prefix;
+
+    /**
+     * Creates an IndexIdPair that can identify either a person or a location.
+     * The {@code ArgumentMultimap} should contain either index or Id user input but not both.
+     * @param argMultimap contains the Id or index user input.
+     * @param prefix determines if this IndexIdPair identifies a person or a location.
+     * @throws ParseException if unable to parse the Id or the index.
+     */
+    public IndexIdPair(ArgumentMultimap argMultimap, Prefix prefix) throws ParseException {
+        requireAllNonNull(argMultimap, prefix);
+        this.prefix = prefix;
+
+        if (argMultimap.getValue(prefix).isPresent()) {
+            Id id = ParserUtil.parseId(argMultimap.getValue(prefix).get());
+            indexOpt = Optional.empty();
+            idOpt = Optional.of(id);
+        }
+
+        Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        indexOpt = Optional.of(index);
+        idOpt = Optional.empty();
+    }
+
+    /**
+     * Checks that {@code ArgumentMultimap} does not contain both Id and Index strings.
+     */
+    public boolean checkIndexOrIdOnly(ArgumentMultimap argMultimap, Prefix prefix) {
+        return argMultimap.getValue(prefix).isPresent() && !argMultimap.getPreamble().isEmpty();
+    }
+
+    /**
+     * Gets the person that is being referred to from the {@code Model}.
+     * @throws CommandException if the person cannot be found from the model
+     *                          or if this pair is referring to a location.
+     */
+    public Person getPersonFromPair(Model model) throws CommandException {
+        requireNonNull(model);
+        if (!prefix.equals(PREFIX_PERSON_ID)) {
+            throw new CommandException(MESSAGE_INVALID_PERSON_COMMAND_USE);
+        }
+
+        if (idOpt.isEmpty()) {
+            List<Person> lastShownList = model.getSortedPersonList();
+            if (indexOpt.get().getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            return model.getPersonFromIndex(indexOpt.get());
+        } else {
+            assert indexOpt.isEmpty();
+            if (!model.hasPersonId(idOpt.get())) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_ID);
+            }
+
+            return model.getPersonById(idOpt.get());
+        }
+    }
+
+    /**
+     * Gets the location that is being referred to from the {@code Model}.
+     * @throws CommandException if the location cannot be found from the model
+     *                          or if this pair is referring to a person.
+     */
+    public Location getLocationFromPair(Model model) throws CommandException {
+        requireNonNull(model);
+        if (!prefix.equals(PREFIX_LOCATION_ID)) {
+            throw new CommandException(MESSAGE_INVALID_LOCATION_COMMAND_USE);
+        }
+
+        if (idOpt.isEmpty()) {
+            List<Location> lastShownList = model.getSortedLocationList();
+            if (indexOpt.get().getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_LOCATION_DISPLAYED_INDEX);
+            }
+
+            return model.getLocationFromIndex(indexOpt.get());
+        } else {
+            assert indexOpt.isEmpty();
+            if (!model.hasLocationId(idOpt.get())) {
+                throw new CommandException(Messages.MESSAGE_INVALID_LOCATION_ID);
+            }
+
+            return model.getLocationById(idOpt.get());
+        }
+    }
+
+    /**
+     * Get the prefix used.
+     * Use this method to identify the object that this Pair is referring to.
+     */
+    public Prefix getPrefix() {
+        return prefix;
+    }
+}
